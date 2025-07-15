339e96534d160e8bedcc8af1f003ade4d1fa4134
Kadu Brum Gavião <kadubgaviao@gmail.com>
2025-07-15 15:43:41 -0300

    Tela de listagem de eventos

--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
+    local = models.CharField(max_length=100, default="A definir")
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
@@ -13,3 +14,6 @@ class Evento(models.Model):
 
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M')
--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Evento
 
-# Create your views here.
+def get_local_evento(request, titulo_evento):
+    try:
+        evento = Evento.objects.get(titulo=titulo_evento)
+        return HttpResponse(evento.local)
+    except Evento.DoesNotExist:
+        return HttpResponse("Evento não encontrado", status=404)
+
+# def index(request):
+#   return redirect('/agenda/')
+
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.all()
+    dados = {'eventos': evento}
+    return render(request, 'agenda.html', dados)
+
