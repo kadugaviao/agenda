fd9f636323df5f55b5a3526d5d38937b58f3ea9c
Kadu Brum Gavião <kadubgaviao@gmail.com>
2025-07-16 00:42:00 -0300

    Função e tela de autenticação

--- agenda/urls.py
+++ agenda/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('eventos/<str:titulo_evento>', views.get_local_evento, name='get_local_evento'),
     path('agenda/', views.lista_eventos),
     path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/') 
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username,password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha inválidos")
+    return redirect('/')
 
 def get_local_evento(request, titulo_evento):
     try:
@@ -12,9 +34,10 @@ def get_local_evento(request, titulo_evento):
 # def index(request):
 #   return redirect('/agenda/')
 
+@login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
-    evento = Evento.objects.all()
+    evento = Evento.objects.filter(usuario=request.user)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
 
