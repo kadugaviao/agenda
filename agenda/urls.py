93e332b9a2e6e371a8da367d1bf5ed5d9a1e9b87
Kadu Brum Gavião <kadubgaviao@gmail.com>
2025-07-21 23:44:25 -0300

    Teste templete para inserção de dados de eventos da agenda. Juntamente com uma modificação do .gitignore

--- agenda/urls.py
+++ agenda/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 from django.views.generic import RedirectView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('eventos/<str:titulo_evento>', views.get_local_evento, name='get_local_evento'),
     path('agenda/', views.lista_eventos),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento, name='submit_evento'),
     path('', RedirectView.as_view(url='/agenda/')),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
