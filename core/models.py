a9be781007ab9605be237ee2e45589da935ac26d
Kadu Brum Gavião <kadubgaviao@gmail.com>
2025-07-23 18:36:50 -0300

    Funções de alteração e exclusão de dados

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
-    local = models.CharField(max_length=100, default="A definir")
+    local = models.CharField(max_length=100, blank=True, null=True)
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
@@ -17,3 +17,6 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y %H:%M')
+    
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
